0edd65cdecaa94405a57e3f1bd712e9978bcf0bd
Mazen  Bakr <71599149+IX0XI@users.noreply.github.com>
2022-10-27 04:42:31 +0200

    Added Quantization And Encoding

--- DSPComponents/Algorithms/QuantizationAndEncoding.cs
+++ DSPComponents/Algorithms/QuantizationAndEncoding.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DSPAlgorithms.DataStructures;
+using Microsoft.SqlServer.Server;
 
 namespace DSPAlgorithms.Algorithms
 {
@@ -21,7 +22,42 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            List<Tuple<float, float>> intervals = new List<Tuple<float, float>>();
+            OutputQuantizedSignal = new Signal(new List<float>(), false);
+            OutputIntervalIndices = new List<int>();
+            OutputEncodedSignal = new List<string>();
+            OutputSamplesError = new List<float>();
+
+            float minAmplitude = InputSignal.Samples.AsQueryable().Min(),
+                  maxAmplitude = InputSignal.Samples.AsQueryable().Max(),
+                   delta;
+            int levelsNum = (InputLevel != 0) ? InputLevel : (int)Math.Pow(2, InputNumBits);
+            delta = (maxAmplitude - minAmplitude) / levelsNum;
+
+            float start = minAmplitude;
+            for (int i = 0; i < levelsNum; i++)
+            {
+                intervals.Add(new Tuple<float, float>((float)Math.Round(start, 2),(float)Math.Round(start + delta, 2)));
+                start+=delta;
+            }
+
+
+            foreach(float element in InputSignal.Samples)
+            {
+                for (int i=0;i<intervals.Count;i++)
+                {
+                    if (element >= intervals[i].Item1 && element <= intervals[i].Item2)
+                    {
+                        float midpoint = (intervals[i].Item1 + intervals[i].Item2) / 2;
+                        OutputQuantizedSignal.Samples.Add(midpoint);
+                        OutputIntervalIndices.Add(i + 1);
+                        OutputSamplesError.Add(midpoint-element);
+                        OutputEncodedSignal.Add(Convert.ToString(i, 2).PadLeft((int)Math.Log(levelsNum, 2), '0'));
+                        break;
+                    }
+                }
+            }
+            
         }
     }
 }
