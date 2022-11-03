cb591275b9c538f93e41b099c7d48d24846cdc59
Mohamed Ibrahim <mohmed42010@gmail.com>
2022-11-04 00:30:06 +0200

    Solved DFT and IDFT

--- DSPComponents/Algorithms/DiscreteFourierTransform.cs
+++ DSPComponents/Algorithms/DiscreteFourierTransform.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DSPAlgorithms.DataStructures;
+using System.Numerics;
+
 
 namespace DSPAlgorithms.Algorithms
 {
@@ -15,7 +17,23 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            OutputFreqDomainSignal = new Signal(true,new List<float>(),new List<float>(),new List<float>());
+            
+            for (int i = 0; i < InputTimeDomainSignal.Samples.Count; i++)
+            {
+                Complex sum = new Complex(0, 0);
+                for (int j = 0; j < InputTimeDomainSignal.Samples.Count; j++)
+                {
+                    Complex e = new Complex(InputTimeDomainSignal.Samples[j] * (float)Math.Cos(2 * Math.PI * i * j / InputTimeDomainSignal.Samples.Count),
+                        -InputTimeDomainSignal.Samples[j] * (float)Math.Sin(2 * Math.PI * i * j / InputTimeDomainSignal.Samples.Count));
+                    sum = new Complex(e.Real + sum.Real, e.Imaginary + sum.Imaginary);
+                }
+                OutputFreqDomainSignal.FrequenciesAmplitudes.Add((float)sum.Magnitude);
+                OutputFreqDomainSignal.FrequenciesPhaseShifts.Add((float)sum.Phase);
+
+            }
+
+
         }
     }
 }
--- DSPComponents/Algorithms/InverseDiscreteFourierTransform.cs
+++ DSPComponents/Algorithms/InverseDiscreteFourierTransform.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using DSPAlgorithms.DataStructures;
@@ -14,7 +15,22 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            OutputTimeDomainSignal = new Signal(new List<float>(), false);
+            // Reconstruct the signal from the given frequency domain samples
+            // Use the following formula to reconstruct the signal:
+            // x[n] = (1/N) * (X[0] + 2 * (X[1] + X[2] + ... + X[N/2 - 1]) + X[N/2]) * e^(j * 2 * pi * n * k / N)
+
+            for (int i = 0; i < InputFreqDomainSignal.Frequencies.Count; i++)
+            {
+                Complex sum = new Complex(0, 0);
+                for (int j = 0; j < InputFreqDomainSignal.Frequencies.Count; j++)
+                {
+                    Complex x = Complex.FromPolarCoordinates(InputFreqDomainSignal.FrequenciesAmplitudes[j], InputFreqDomainSignal.FrequenciesPhaseShifts[j]);
+                    sum += x * Complex.Exp(new Complex(0, 2 * Math.PI * i * j / InputFreqDomainSignal.Frequencies.Count));
+                }
+                OutputTimeDomainSignal.Samples.Add((float)sum.Real / InputFreqDomainSignal.Frequencies.Count);
+
+            }
         }
     }
 }
